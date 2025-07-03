851c87883d5017b587bb84531081be86e7d128d1
Matthias Wuketich <matthias.wuketich@bnet.at>
2025-07-03 18:25:52 +0200

    improved error message for for unit structs
    when deriving bundle

--- crates/bevy_macro_utils/src/shape.rs
+++ crates/bevy_macro_utils/src/shape.rs
@@ -1,29 +1,25 @@
-use syn::{
-    punctuated::Punctuated, spanned::Spanned, token::Comma, Data, DataEnum, DataUnion, Error,
-    Field, Fields,
-};
+use proc_macro::Span;
+use syn::{punctuated::Punctuated, token::Comma, Data, DataStruct, Error, Field, Fields};
 
 /// Get the fields of a data structure if that structure is a struct with named fields;
 /// otherwise, return a compile error that points to the site of the macro invocation.
-///
-/// `meta` should be the name of the macro calling this function.
-pub fn get_struct_fields<'a>(
-    data: &'a Data,
-    meta: &str,
-) -> syn::Result<&'a Punctuated<Field, Comma>> {
-    match data {
-        Data::Struct(data_struct) => match &data_struct.fields {
-            Fields::Named(fields_named) => Ok(&fields_named.named),
-            Fields::Unnamed(fields_unnamed) => Ok(&fields_unnamed.unnamed),
-            Fields::Unit => Ok(const { &Punctuated::new() }),
-        },
-        Data::Enum(DataEnum { enum_token, .. }) => Err(Error::new(
-            enum_token.span(),
-            format!("#[{meta}] only supports structs, not enums"),
-        )),
-        Data::Union(DataUnion { union_token, .. }) => Err(Error::new(
-            union_token.span(),
-            format!("#[{meta}] only supports structs, not unions"),
+pub fn get_struct_fields(data: &Data) -> syn::Result<&Punctuated<Field, Comma>> {
+    let Data::Struct(DataStruct { fields, .. }) = data else {
+        return Err(Error::new(
+            // This deliberately points to the call site rather than the structure
+            // body; marking the entire body as the source of the error makes it
+            // impossible to figure out which `derive` has a problem.
+            Span::call_site().into(),
+            "Only structs are supported",
+        ));
+    };
+
+    match fields {
+        Fields::Named(fields) => Ok(&fields.named),
+        Fields::Unnamed(fields) => Ok(&fields.unnamed),
+        Fields::Unit => Err(Error::new(
+            Span::mixed_site().into(),
+            "Unit structs are not supported. Try using an empty Struct",
         )),
     }
 }
